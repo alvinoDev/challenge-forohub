0bdea962743b6298b59cb08247a32170505c42e1
Jhonny Alvino C. <48383924+alvinoDev@users.noreply.github.com>
2026-01-15 11:44:06 -0400

    feat: agregar archivos base del proyecto

---
+++ src/test/java/alvino/dev/challenge_forohub/ChallengeForohubApplicationTests.java
@@ -0,0 +1,13 @@
+package alvino.dev.challenge_forohub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeForohubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
