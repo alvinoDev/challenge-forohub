defceb71236ead67dd528f14fc22bc32da5852f5
Jhonny Alvino C. <48383924+alvinoDev@users.noreply.github.com>
2026-01-17 15:38:25 -0400

    feat: agregar DTO de registr (DatosRegistroTopico) y respuesta (DatosRespuestaTopico) para evitar exponer la entidad directamente en API

---
+++ src/main/java/alvino/dev/challenge_forohub/domain/topico/DatosRespuestaTopico.java
@@ -0,0 +1,25 @@
+package alvino.dev.challenge_forohub.domain.topico;
+
+import java.time.LocalDateTime;
+
+public record DatosRespuestaTopico(
+        Long id,
+        String titulo,
+        String mensaje,
+        LocalDateTime fechaCreacion,
+        EstadoTopico status,
+        Long autorId,
+        Long cursoId
+) {
+    public DatosRespuestaTopico(Topico topico) {
+        this(
+            topico.getId(),
+            topico.getTitulo(),
+            topico.getMensaje(),
+            topico.getFechaCreacion(),
+            topico.getStatus(),
+            topico.getAutorId(),
+            topico.getCursoId()
+        );
+    }
+}
