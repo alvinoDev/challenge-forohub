20db607a5874f6adf6748fae1e38015d3ea0dd84
Jhonny Alvino C. <48383924+alvinoDev@users.noreply.github.com>
2026-01-26 11:31:09 -0400

    feat(Autenticación): crear Servicio para Tokens JWT

---
+++ src/main/java/alvino/dev/challenge_forohub/security/TokenService.java
@@ -0,0 +1,56 @@
+package alvino.dev.challenge_forohub.security;
+
+import alvino.dev.challenge_forohub.domain.usuario.Usuario;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("${challenge.forohub.security.jwt.expiration}") // En application.properties: api.security.secret=tu-secreto-largo
+    private String secret;
+
+    @Value("${challenge.forohub.security.jwt.expiration}")
+    private Duration expirationTime;
+
+    public String generateToken(Usuario usuario) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("forohub")
+                    .withSubject(usuario.getCorreoElectronico())
+                    .withClaim("id", usuario.getId())
+                    .withExpiresAt(generarFechaExpiracion())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Error al generar token");
+        }
+    }
+
+    public String getSubject(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("forohub")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            return null;
+        }
+    }
+
+    private Instant generarFechaExpiracion() {
+        return Instant.now().plus(expirationTime);
+    }
+}
