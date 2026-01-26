a93157e021205fa594a0219f954d5e29f2943d26
Jhonny Alvino C. <48383924+alvinoDev@users.noreply.github.com>
2026-01-26 11:34:19 -0400

    feat(Autenticación): crear el Filtro de JWT (extiende OncePerRequestFilter)

---
+++ src/main/java/alvino/dev/challenge_forohub/security/SecurityFilter.java
@@ -0,0 +1,43 @@
+package alvino.dev.challenge_forohub.security;
+
+import alvino.dev.challenge_forohub.domain.usuario.AutenticacionService;
+import alvino.dev.challenge_forohub.domain.usuario.UsuarioRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.replace("Bearer ", "");
+            String subject = tokenService.getSubject(token);
+            if (subject != null) {
+                // Buscamos directamente en el repositorio
+                var usuario = usuarioRepository.findByCorreoElectronico(subject)
+                        .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+
+                var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
